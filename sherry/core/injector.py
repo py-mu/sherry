1005e1dbc759459ef3072b8d7d9d8f5e0702e105
木豆 <1540235670@qq.com>
2021-07-15 19:34:04 +0800

    分离注射器

--- sherry/core/injector.py
+++ sherry/core/injector.py
@@ -8,19 +8,14 @@ from __future__ import print_function
 
 import functools
 
-from PyQt5.QtCore import Qt, QEvent, QRect
-from PyQt5.QtWidgets import QToolTip, QWidget, QAbstractScrollArea
-
 __all__ = ('WidgetInjector', 'register')
-__instance_func__ = []
-
-from sherry.core.reflex import ReflexCenter
+instance_func = []
 
 
 def register(func):
     """注册方法"""
 
-    __instance_func__.append(func)
+    instance_func.append(func)
 
     @functools.wraps(func)
     def warps():
@@ -35,71 +30,6 @@ class WidgetInjector:
         self.__install()
 
     @staticmethod
-    def _register_tooltip(obj):
-        """替换内置的tooltip样式"""
-        primeval_event_function = getattr(obj, 'event')
-
-        def graft(widget, event):
-            if event.type() == QEvent.ToolTip and widget.toolTip():
-                QToolTip.showText(event.globalPos(), "这是重载后的 tooltip", widget, QRect(),
-                                  widget.toolTipDuration())
-                return True
-            return primeval_event_function(widget, event)
-
-        return graft
-
-    @staticmethod
-    @register
-    def install_events():
-        """装载 event 颗粒"""
-        ReflexCenter.hook(QWidget, 'event', WidgetInjector._register_tooltip(QWidget))
-        ReflexCenter.hook(QAbstractScrollArea, 'event', WidgetInjector._register_tooltip(QAbstractScrollArea))
-
-    @staticmethod
-    @register
-    def install_enter_event():
-        primeval_func = getattr(QWidget, "enterEvent")
-
-        def enterEvent(widget, event):
-            if not hasattr(widget, 'raw_cursor'):
-                ReflexCenter.hook(widget, 'raw_cursor', Qt.ArrowCursor)
-            if widget.parent():
-                ReflexCenter.hook(widget, 'raw_cursor', widget.parent().cursor())
-                if not widget.isEnabled():
-                    widget.parent().setCursor(Qt.ForbiddenCursor)
-            return primeval_func(widget, event)
-
-        ReflexCenter.hook(QWidget, 'enterEvent', enterEvent)
-
-    @staticmethod
-    @register
-    def install_leave_event():
-        primeval_func = getattr(QWidget, "leaveEvent")
-
-        def leaveEvent(widget, event) -> None:
-            if not hasattr(widget, 'raw_cursor'):
-                ReflexCenter.hook(widget, 'raw_cursor', Qt.ArrowCursor)
-            if widget.parent():
-                widget.parent().setCursor(widget.raw_cursor)
-            return primeval_func(widget, event)
-
-        ReflexCenter.hook(QWidget, 'leaveEvent', leaveEvent)
-
-    #  设置动态修改属性会重载样式
-    @staticmethod
-    @register
-    def install_set_property():
-        primeval_func = getattr(QWidget, "setProperty")
-
-        def setProperty(self, name, value):
-            """在属性发生变化时刷新样式(不允许下划线开头)"""
-            result = primeval_func(self, name, value)
-            self.style().polish(self)
-            return result
-
-        ReflexCenter.hook(QWidget, 'setProperty', setProperty)
-
-    @staticmethod
     def __install():
-        for func in __instance_func__:
+        for func in instance_func:
             func()
--- sherry/core/injector.pyi
+++ sherry/core/injector.pyi
@@ -4,39 +4,16 @@
     on 2021/6/14
     at 2:43
 """
-from typing import List, Callable, Type
+from typing import List, Callable
 
 __all__ = ('WidgetInjector', 'register')
-__instance_func__: List[Callable[[], None]] = []
-
-from PyQt5.QtCore import QEvent
-from PyQt5.QtWidgets import QWidget
+instance_func: List[Callable[[], None]] = []
 
 
-def register(func: Callable): ...
+def register(func: Callable[[], None]): ...
 
 
 class WidgetInjector:
-
-    @staticmethod
-    def _register_tooltip(obj: Type[QWidget]) -> Callable[[QWidget, QEvent], bool]: ...
-
-    @staticmethod
-    @register
-    def install_events(): ...
-
-    @staticmethod
-    @register
-    def install_enter_event(): ...
-
-    @staticmethod
-    @register
-    def install_leave_event(): ...
-
-    #  设置动态修改属性会重载样式
-    @staticmethod
-    @register
-    def install_set_property(): ...
 
     @staticmethod
     def __install(): ...
