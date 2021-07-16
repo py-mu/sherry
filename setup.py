0cba6ae75ae171189f792d3c1ad53cfbcf41f5c7
木豆 <1540235670@qq.com>
2021-07-16 15:57:29 +0800

    修改文档

--- setup.py
+++ setup.py
@@ -10,20 +10,11 @@ from setuptools import setup, find_packages
 
 from sherry import __sherry_info__
 
-# 分类信息
 classifiers = [
-    # 发展时期,常见的如下
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
     'Development Status :: 3 - Alpha',
-    # 开发的目标用户
     'Intended Audience :: Developers',
-    # 属于什么类型
     'Topic :: Software Development :: Build Tools',
-    # 许可证信息
     'License :: OSI Approved :: MIT License',
-    # 目标 Python 版本
     'Programming Language :: Python :: 3',
 ]
 
