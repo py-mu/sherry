038f8dd572ca1cd282cb454e55f358471a7dd2a0
木豆 <1540235670@qq.com>
2021-07-20 10:49:55 +0800

    upload pypi

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
+    long_description_content_type="text/markdown",
     long_description=long_description,
     platforms=['OS-independent'],
     classifiers=classifiers,
